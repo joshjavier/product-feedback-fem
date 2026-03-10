7e0bd056eaea5a5b202ed6ab294a75e968b471aa
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2026-03-11 05:29:44 +0800

    feat: implement sort picker

--- components/sort-picker/index.tsx
+++ components/sort-picker/index.tsx
@@ -1,18 +1,21 @@
 'use client';
 
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 export default function SortPicker() {
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
+  const [sort, setSort] = useState<string>('upvotes');
+  const [order, setOrder] = useState<string>('desc');
+  const [shouldRender, setShouldRender] = useState(false);
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const [sort, order] = e.target.value.split(':');
     const params = new URLSearchParams(searchParams.toString());
 
-    if (sort === 'upvotes' && order === 'desc') {
+    if (sort === 'upvotes') {
       params.delete('sort');
     } else {
       params.set('sort', sort);
@@ -25,11 +28,27 @@ export default function SortPicker() {
     }
 
     const href = pathname + '?' + params.toString();
+
+    setSort(sort);
+    setOrder(order);
     router.push(href);
   };
 
+  useEffect(() => {
+    const initialSort = searchParams.get('sort') || 'upvotes';
+    const initialOrder = searchParams.get('asc') == null ? 'desc' : 'asc';
+
+    setSort(initialSort);
+    setOrder(initialOrder);
+    setShouldRender(true);
+  }, []);
+
+  if (!shouldRender) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <select name="sort" id="sort-select" onChange={handleChange}>
+    <select name="sort" id="sort-select" value={`${sort}:${order}`} onChange={handleChange}>
       <option value="upvotes:desc">Most Upvotes</option>
       <option value="upvotes:asc">Least Upvotes</option>
       <option value="comments:desc">Most Comments</option>
